f7d91f4f440bded0a147943c0e207819719c7f9b
BatoulBlaybel <blbatoul85@gmail.com>
2025-05-22 22:47:38 +0300

    Added HashRouter for GitHub Pages routing and updated homepage

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// ✅ Add this line
+import { HashRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 )
+
